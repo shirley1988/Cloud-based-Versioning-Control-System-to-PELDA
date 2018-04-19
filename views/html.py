d8c9073d7b60fec4f6fe20bdb1a58a13b17e89ec
Nana Liu <shirley19881218@gmail.com>
2018-04-19 12:24:12 -0700

    login

--- views/html.py
+++ views/html.py
@@ -1,9 +1,73 @@
-from flask import send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect, url_for, request, session, json
+from flask_cors import CORS
+import string
+import pymysql
+import subprocess
+import bcrypt
 from praat import app
+
+database_host = 'peldaw2018.mysql.database.azure.com'
+database_user = 'myadmin@peldaw2018'
+database_password = '1988@Idaho'
+database_db = 'peldaw'
+
+conn = pymysql.connect(user = database_user, 
+        password = database_password, 
+        database = database_db, 
+        host = database_host)
 
 @app.route('/')
 def index():
-   return app.send_static_file("index.html")
+	if 'username' in session:
+		return app.send_static_file("index.html")
+	return render_template('home.html')
+
+@app.route('/logout')
+def logout():
+    if 'username' in session:
+        session['username'] = None
+    return render_template('home.html')
+
+@app.route('/login',methods=['POST'])
+def login():
+    cursor = conn.cursor()
+    username = request.form['username']
+    cursor.execute("SELECT * FROM user WHERE username = %s;", username)
+    users = cursor.fetchall()
+    print(users)
+
+    if users:
+        #hash_password = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+        if username == users[0][0]:
+            session['username'] = username
+            conn.commit()
+            #return app.send_static_file("index.html")
+            return redirect(url_for('index'))
+
+    conn.commit()
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        cursor = conn.cursor()
+        username = request.form['username']
+        cursor.execute("SELECT * FROM user WHERE username = %s;", username)
+        users = cursor.fetchall()
+        print(users)
+        print(len(users))
+
+        if len(users) == 0:
+            hash_password = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            cursor.execute("INSERT INTO user (username, password) VALUES (%s, %s);", (username, hash_password))
+            session['username'] = username
+            conn.commit()
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
 
 @app.route('/praatapidocs')
 def Praatapidocs():
